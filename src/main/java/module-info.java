1615baaa17c1e5110375df95cc39a5bd7936b4aa
bacn <mbachmann@trdi.io>
2021-04-12 16:54:07 +0200

    update module info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,5 +3,6 @@ module ch.makery.address {
     requires javafx.fxml;
 
     opens ch.makery.address to javafx.fxml;
+    opens ch.makery.address.view to javafx.fxml;
     exports ch.makery.address;
 }
